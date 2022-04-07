8ceb2936377b5fc7247ff33093e2dfecae8c7a41
ayyaz10 <codementee@outlook.com>
2022-04-07 19:52:33 +0500

    update

--- server.js
+++ server.js
@@ -24,15 +24,15 @@ app.use(cors());
 //   }
 // });
 
-// const db = knex ({
-//   client: 'pg',
-//   connection: {
-//     connectionString: process.env.DATABASE_URL,
-//     ssl: {
-//       rejectUnauthorized: false
-//     }
-//   }
-// });
+const db = knex ({
+  client: 'pg',
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  }
+});
 
 
 
