bf97dc1a19eacf8e18a1214fc03729a2d904dc28
ayyaz10 <codementee@outlook.com>
2022-04-07 07:48:15 +0500

    update

--- server.js
+++ server.js
@@ -3,26 +3,22 @@ const {
 } = require('uuid');
 const express = require('express');
 // var path = require("path");
-// const cors = require('cors');
-// const ExcelJS = require('exceljs');
+const cors = require('cors');
 const xlsx = require('xlsx');
 const app = express();
 
 app.use(express.json());
-// app.use(router);
 app.use(express.urlencoded({
   extended: true
 }));
-// app.use(cors());
+app.use(cors());
 
 function addDataToExcel(userData) {
-  // console.log(userData)
   const wb = xlsx.readFile('./assets/userdata.xlsx');
   const ws = wb.Sheets['userdata'];
   const data = xlsx.utils.sheet_to_json(ws)
   // console.log(ws)
   data.push(userData)
-  // console.log(data)
   const newWorkbook = xlsx.utils.book_new();
   const newws = xlsx.utils.json_to_sheet(data);
   xlsx.utils.book_append_sheet(newWorkbook, newws, "userdata");
@@ -38,14 +34,11 @@ app.get('/', function (req, res) {
 app.post('/emaildata', function (req, res) {
   try {
     const userData = req.body.jsonFormData;
-    // console.log(userData)
     addDataToExcel(userData);
     res.status(200).send({
       isSuccess: true
     })
   } catch (error) {
-    console.log('error is happened')
-    // console.error(error);
     res.status(500).send({
       isSuccess: false
     })
