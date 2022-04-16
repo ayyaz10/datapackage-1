ecc46edc53e7d777256bae87416d6b1b894c7135
ayyaz10 <codementee@outlook.com>
2022-04-16 23:04:10 +0500

    update

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({
 //   next();
 // })
 var corsOptions = {
-  origin: 'http://example.com',
+  origin: 'https://ayyaz10.github.io/data-collection/',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 // app.use(cors());
