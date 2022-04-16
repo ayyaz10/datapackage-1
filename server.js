a251800afbf67149768cb91778a883ed2a71f33e
ayyaz10 <codementee@outlook.com>
2022-04-16 21:50:37 +0500

    update

--- server.js
+++ server.js
@@ -14,6 +14,12 @@ app.use(express.json());
 app.use(express.urlencoded({
   extended: true
 }));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "origin, X-Reqeusted-With, Content-Type, Accept");
+  next();
+})
 // app.use(cors());
 // console.log(moment().format('D/M/YYYY'))
 // const db = knex({
