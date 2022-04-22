425d2c75d13d5918d239bd42417c3105e734ad68
ayyaz10 <codementee@outlook.com>
2022-04-23 03:47:14 +0500

    init

--- server.js
+++ server.js
@@ -16,11 +16,11 @@ app.use(express.json());
 app.use(express.urlencoded({
   extended: false
 }));
-const corsOptions = {
-  origin: '*',
-  optionsSuccessStatus: 200,
-}
-app.use(cors(corsOptions))
+// const corsOptions = {
+//   origin: '*',
+//   optionsSuccessStatus: 200,
+// }
+// app.use(cors(corsOptions))
 
 // app.use(function(req, res, next) {
 //   res.header("Access-Control-Allow-Origin", "*");
@@ -45,7 +45,7 @@ app.use(cors(corsOptions))
 //   origin: "https://test.microstun.com",
 //   methods: ["GET", "POST"]
 // }));
-// app.use(cors("*"));
+app.use(cors());
 // app.use(cors({
 //   origin: ['https://test.microstun.com'],
 //   credentials: true
