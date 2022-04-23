539800f09118521612b2ea3f427419767e112bef
ayyaz10 <codementee@outlook.com>
2022-04-23 23:24:22 +0500

    update

--- server.js
+++ server.js
@@ -25,110 +25,55 @@ const db = knex ({
 app.get('/', async function (req, res) {
   res.send('working')
 })
+// Khalistan fund router /sfjkhuserdata
+app.post('/sfjkhuserdata', async function (req, res) {
+  console.log(req.body)
+  const { name, address, mobile, email, tlamount, nameofbank } = req.body;
 
-
-
-// Voter website router /voterdata
-function assignVoterId(id, numToAdd) {
-  const obj = {};
-  function addZeros(preZeroId) {
-    let zeroAddedId;
-    if (preZeroId <= 9) {
-      zeroAddedId = `0000${preZeroId}`;
-    }else if (preZeroId > 9 && preZeroId <= 99) {
-      zeroAddedId = `000${preZeroId}`;
-    } else if (preZeroId > 99 && preZeroId <= 999) {
-      zeroAddedId = `00${preZeroId}`;
-    } else if (preZeroId > 999 && preZeroId <= 9999) {
-      zeroAddedId = `0${preZeroId}`;
-    } else {
-      zeroAddedId = `${preZeroId}`;
-    }
-     obj.id = zeroAddedId;
-  }
-    if (id) {
-      if (numToAdd === 3) {
-        
-        id = id + numToAdd;
-        addZeros(id);
-        numToAdd = 5;
-    
-      } else if (numToAdd === 5) {
-        id = id + numToAdd;
-        addZeros(id);
-        numToAdd = 7;
-    
-      } else {
-        id = id + numToAdd;
-        addZeros(id);
-        numToAdd = 3;
-    
-      }
-    } else {
-      id = 1;
-      addZeros(id);
-      numToAdd = 3;
-  
-    }
-    // obj.id = id;
-    obj.numToAdd = numToAdd;
-    return obj;
-  
-  }
-  
-  async function insertNumToAdd(number) {
-    const dbNumToAdd = await getDbNumToAdd();
-    if(typeof dbNumToAdd == 'undefined') {
-      const dbNum = db('num_to_add').insert({
-        number
-       })
-      return dbNum;
-    } else {
-      const res = await db('num_to_add').update({number});
-      // console.log(res)
-    }
-  }   
-  
-  async function getDbVoterId() {
-     const dbData = await db.select().from('pk_userdata').orderBy([{column:"id",order:"desc"}]).limit(1)
-     if(dbData.length < 1) {
-      return;
-     }
-     return dbData[0].voter_id;
-  }
-  // HAVING ISSUE WITH 9
-  async function getDbNumToAdd() {
-     const dbNumToAdd = await db.select("number").table('num_to_add');
-     if(dbNumToAdd.length < 1) {
-      return;
-     } 
-       return dbNumToAdd[0].number;
-  }
-  
-  app.post('/voterdata', async function(req, res) {
-    const { name, mobile, email } = req.body;
-    const dbVoterId = await getDbVoterId();
-    const dbNumToAdd = await getDbNumToAdd();
-    const voterData = assignVoterId(Number(dbVoterId), Number(dbNumToAdd));
-    const dbNum = insertNumToAdd(voterData.numToAdd);
-    db('pk_userdata').insert({
-    voter_id: voterData.id,
-    name: name,
-    mobile: mobile,
-    email: email,
-    created: new Date(),
-  })
-    .then(dbres => {
-      res.status(200).send({
-        voterId: voterData.id,
-        isSuccess: true
+  try {
+    if(!validator.isMobilePhone(mobile)) {
+      res.send({
+        isSuccess: false,
+        msg: "Please enter a valid Mobile Number"
       })
-    })
-    // const databaseRes = await assignVoterId(req)
-    // res.send('ok')
-  })
-  // ****************
+      return
+    }
+    if(!validator.isEmail(email)) {
+      res.send({
+        isSuccess: false,
+        msg: "Please enter a valid Email Address"
+      })
+      return
+    }
 
+    if(!validator.isEmpty(address) && !validator.isEmpty(mobile) && !validator.isEmpty(email) && !validator.isEmpty(tlamount) && !validator.isEmpty(nameofbank)) {
+      db('sfj_kh4fm_userdata').insert({
+        name,
+        address,
+        mobile,
+        email,
+        tlamount,
+        nameofbank,
+        created: new Date(),
+      })
+      .then(data => {
+        // console.log(data)
+        res.status(200).send({
+          isSuccess: true
+        })
+      })
+    } else {
+      res.send({
+        isSuccess: false,
+        msg: "Please fill the empty fields"
+      })
+    }
+  } catch (error) {
+    res.status(500).send({
+      isSuccess: false
+    })
+  }
+})
 
 const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => {
