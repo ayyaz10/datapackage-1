9a5e7b28dddc9168967f77ddc22feae7a8a5a2fd
ayyaz10 <codementee@outlook.com>
2022-04-23 23:02:34 +0500

    update

--- server.js
+++ server.js
@@ -129,54 +129,6 @@ function assignVoterId(id, numToAdd) {
   })
   // ****************
 
-  app.post('/sfjkhuserdata', cors({origin: "*"}), async function (req, res) {
-    console.log(req.body)
-    const { name, address, mobile, email, tlamount, nameofbank } = req.body;
-  
-    try {
-      if(!validator.isMobilePhone(mobile)) {
-        res.send({
-          isSuccess: false,
-          msg: "Please enter a valid Mobile Number"
-        })
-        return
-      }
-      if(!validator.isEmail(email)) {
-        res.send({
-          isSuccess: false,
-          msg: "Please enter a valid Email Address"
-        })
-        return
-      }
-  
-      if(!validator.isEmpty(address) && !validator.isEmpty(mobile) && !validator.isEmpty(email) && !validator.isEmpty(tlamount) && !validator.isEmpty(nameofbank)) {
-        db('sfj_kh4fm_userdata').insert({
-          name,
-          address,
-          mobile,
-          email,
-          tlamount,
-          nameofbank,
-          created: new Date(),
-        })
-        .then(data => {
-          // console.log(data)
-          res.status(200).send({
-            isSuccess: true
-          })
-        })
-      } else {
-        res.send({
-          isSuccess: false,
-          msg: "Please fill the empty fields"
-        })
-      }
-    } catch (error) {
-      res.status(500).send({
-        isSuccess: false
-      })
-    }
-  })
 
 const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => {
